577d7b56a77fc5d8d064dbd7c652810d0f5f7f61
Catalyze326 <cfmcool@gmail.com>
2019-02-17 18:26:10 -0500

    Merge

--- main.py
+++ main.py
@@ -16,6 +16,9 @@ try:
 except ImportError:
     from xml.etree.ElementTree import XML
 import zipfile
+from flask import Flask
+from flask import render_template
+
 
 WORD_NAMESPACE = '{http://schemas.openxmlformats.org/wordprocessingml/2006/main}'
 PARA = WORD_NAMESPACE + 'p'
@@ -141,87 +144,128 @@ def make_threads(phrase_list, threadingCounter):
     return threadingCounter - threadingCounterDefault
 
 
-time1 = time.time()
-s = ""
+def main():
+    time1 = time.time()
+    s = ""
+    try:
+        if (sys.argv[1][-4:].lower() == ".pdf"):
+            try:
+                # If it is a pdf that does not contain pure text add the true flag
+                if sys.argv[2] == "--true":
+                    try:
+                        if sys.argv[3] == "--true":
+                            # Convets the pdf into a string of images
+                            images = convert_from_path(sys.argv[1])
+                            threadingCounter = 0
+                            ''' Turns the images into text and them into small enough sizes
+                            to send to google and then multi threads the process of sending
+                            the strings to google than saves those files to mp3s'''
+                            f = open("output.txt", 'w')
+                            for i in range(len(images)):
+                                f.write(image_pdf(images[i], i))
 
-try:
-    if (sys.argv[1][-4:].lower() == ".pdf"):
-        try:
-            # If it is a pdf that does not contain pure text add the true flag
-            if sys.argv[2] == "--true":
-                try:
-                    if sys.argv[3] == "--true":
+                    except IndexError:
                         # Convets the pdf into a string of images
                         images = convert_from_path(sys.argv[1])
                         threadingCounter = 0
                         ''' Turns the images into text and them into small enough sizes
                         to send to google and then multi threads the process of sending
                         the strings to google than saves those files to mp3s'''
-                        f = open("output.txt", 'w')
                         for i in range(len(images)):
-                            f.write(image_pdf(images[i], i))
+                            ph = make_phrases(image_pdf(images[i], i))
+                            threadingCounter += make_threads(ph, threadingCounter)
 
-                except IndexError:
-                    # Convets the pdf into a string of images
-                    images = convert_from_path(sys.argv[1])
-                    threadingCounter = 0
-                    ''' Turns the images into text and them into small enough sizes
-                    to send to google and then multi threads the process of sending
-                    the strings to google than saves those files to mp3s'''
-                    for i in range(len(images)):
-                        ph = make_phrases(image_pdf(images[i], i))
-                        threadingCounter += make_threads(ph, threadingCounter)
+            except IndexError:
+                print("It is a normal pdf file")
+                pdfReader = PyPDF2.PdfFileReader(open(sys.argv[1], 'rb'))
+                '''Extracts the text from the pdf, splits it into small enough
+                pieces for it to go to google and then multithreads the sending of
+                the files to google and saves the replys from google to mp3s'''
+                threadingCounter = 0
+                for i in range(pdfReader.numPages):
+                    # Better for debuging
+                    s = str(pdfReader.getPage(i).extractText())
+                    ph = make_phrases(s)
+                    threadingCounter += make_threads(ph, threadingCounter)
+                    # Fewer lines
+                    # make_threads(make_phrases(str(pdfReader.getPage(i).extractText())), 0)
 
-        except IndexError:
-            print("It is a normal pdf file")
-            pdfReader = PyPDF2.PdfFileReader(open(sys.argv[1], 'rb'))
-            '''Extracts the text from the pdf, splits it into small enough
-            pieces for it to go to google and then multithreads the sending of
-            the files to google and saves the replys from google to mp3s'''
-            threadingCounter = 0
-            for i in range(pdfReader.numPages):
-                # Better for debuging
-                s = str(pdfReader.getPage(i).extractText())
-                ph = make_phrases(s)
-                threadingCounter += make_threads(ph, threadingCounter)
-                # Fewer lines
-                # make_threads(make_phrases(str(pdfReader.getPage(i).extractText())), 0)
+        elif sys.argv[1][-4:] == ".txt":
+            # Better for debuging. Make into one line when done
+            '''Converts the text into small enough pieces to send to google, multi
+             threads the sending of those files to google and then saves those files
+             google exports to mp3s'''
+            f = open(sys.argv[1], "r")
+            ph = make_phrases(f.read())
+            make_threads(ph, 0)
 
-    elif sys.argv[1][-4:] == ".txt":
-        # Better for debuging. Make into one line when done
-        '''Converts the text into small enough pieces to send to google, multi
-         threads the sending of those files to google and then saves those files
-         google exports to mp3s'''
-        f = open(sys.argv[1], "r")
-        ph = make_phrases(f.read())
-        make_threads(ph, 0)
-
-    elif sys.argv[1][-5:] == ".docx":
-#       Better for debuging when done put it in one function call
-        '''Extracts the text out of the docx file and then splits that into
-        phrases small enough to send to google and then multithreads that
-        process and then saves the exported files as mp3s'''
-        text = get_docx_text(sys.argv[1])
-        ph = make_phrases()
-        make_threads(ph, 0)
-    elif sys.argv[1][-4:] == ".jpg" or sys.argv[1][-4:] == ".png":
-        print(image_to_string(sys.argv[1]))
-        f = open("output.txt", 'w')
-        f.write(image_to_string(sys.argv[1]))
-        convert_txt_to_docx()
-
-    try:
-        if sys.argv[3] == "--true":
-            print("The file has been exported to a text file")
+        elif sys.argv[1][-5:] == ".docx":
+    #       Better for debuging when done put it in one function call
+            '''Extracts the text out of the docx file and then splits that into
+            phrases small enough to send to google and then multithreads that
+            process and then saves the exported files as mp3s'''
+            text = get_docx_text(sys.argv[1])
+            ph = make_phrases()
+            make_threads(ph, 0)
+        elif sys.argv[1][-4:] == ".jpg" or sys.argv[1][-4:] == ".png":
+            print(image_to_string(sys.argv[1]))
+            f = open("output.txt", 'w')
+            f.write(image_to_string(sys.argv[1]))
             convert_txt_to_docx()
-    except IndexError:
-        generate_single_track()
 
-except KeyboardInterrupt:
-    print("Goodbye World\n")
+        try:
+            if sys.argv[3] == "--true":
+                print("The file has been exported to a text file")
+                convert_txt_to_docx()
+        except IndexError:
+            generate_single_track()
 
-# It causes errors when the leftover files are still there when it runs again
-delete_old_files()
+    except KeyboardInterrupt:
+        print("Goodbye World\n")
 
-time2 = time.time()
-print ("Time taken is " + str(time2 - time1) + " seconds.")
+    # It causes errors when the leftover files are still there when it runs again
+    delete_old_files()
+
+    time2 = time.time()
+    print ("Time taken is " + str(time2 - time1) + " seconds.")
+
+# app = Flask(__name__)
+#
+# if __name__ == "__main__":
+# 	app.run()
+#
+#
+#
+# # @app.route('/hello/')
+# # @app.route('/hello/<name>')
+# # def output():
+# # 	return "Hello World!"
+#
+# @app.route("/output")
+# def output():
+# 	return "Hello World!"
+
+# @app.route('/hello/')
+# @app.route('/hello/<name>')
+# def hello(name=None):
+#     with app.app_context():
+#         render_template('hello.html', name=name)
+
+# @app.route('/hello/')
+# def contact():
+#     print("lol")
+#     if request.method == 'POST':
+#         if request.form['home'] == 'Home':
+#             print("lol")
+#         elif request.form['login'] == 'Login':
+#             pass # do something else
+#         else:
+#             pass # unknown
+#     elif request.method == 'GET':
+#         return render_template('hello.html', form=form)
+
+
+print("hi")
+# output()
+# t1 = threading.Thread(target=, args=(phrase_list[i], threadingCounter,))
+# t2 = threading.Thread(target=contact, args=())
