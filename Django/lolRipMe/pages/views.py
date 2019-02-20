9c5105bf4ee3063fa28528282ef119f33e2e4a95
Catalyze326 <cfmcool@gmail.com>
2019-02-19 20:07:21 -0500

    Major Upgrades, but it is also majorly broken.

--- Django/lolRipMe/pages/views.py
+++ Django/lolRipMe/pages/views.py
@@ -7,6 +7,10 @@ from django.http import Http404
 from django.shortcuts import redirect
 from numba import jit
 
+import pytesseract
+import cv2
+import re
+import concurrent.futures
 import glob
 from gtts import gTTS
 import os
@@ -19,6 +23,7 @@ from pdf2image import convert_from_path
 from PIL import Image
 from pytesseract import image_to_string
 import multiprocessing
+from multiprocessing import Process
 try:
     from xml.etree.cElementTree import XML
 except ImportError:
@@ -70,13 +75,23 @@ def ask_google(string, i):
     tts.save("/home/c/github/Text-to-Speech/Django/lolRipMe/media/unprocessed-audio/piece" + str(i) + ".mp3")
 
 
-# Save the image and then turn it to text
-def image_pdf(image, i):
-    image.save("/home/c/github/Text-to-Speech/images/test" + str(i) + ".jpg")
-    image = Image.open("/home/c/github/Text-to-Speech/images/test" + str(i) + ".jpg", mode='r')
-    # print(image_to_string(image))
-    return image_to_string(image)
+def image_to_text(images, img_path):
+    out_dir = "/home/c/github/Text-to-Speech/Django/lolRipMe/media/ocr-images/"
+    threads = multiprocessing.cpu_count()
+    with concurrent.futures.ProcessPoolExecutor(max_workers=threads) as executor:
+        for img_path,out_file in zip(images,executor.map(ocr,images)):
+            print(img_path.split("\\")[-1],',',out_file,', processed')
 
+
+def ocr(img_path):
+    out_dir = "ocr_results//"
+    img = cv2.imread(img_path)
+    text = pytesseract.image_to_string(img,lang='eng',config='--psm 6')
+    out_file = re.sub(".png",".txt",img_path.split("\\")[-1])
+    out_path = out_dir + out_file
+    fd = open(out_path,"w")
+    fd.write("%s" %text)
+    return out_file
 
 # Turn the large body of text into small pieces
 def make_phrases(s):
@@ -93,103 +108,37 @@ def make_phrases(s):
 
 # Multithread it
 def make_threads(phrase_list, threadingCounter):
+    time_start = time.time()
     threadingCounterDefault = threadingCounter
     length = len(phrase_list)
     i = 0
     cores = multiprocessing.cpu_count()
-    print (length)
+    print ("The ammount of threads we will be making is" + str(length))
+    # procs = []
     while i != length:
         threads = threading.activeCount()
-        # print ("THe threads are " + threads + "\n" + "The cores are " + cores)
         print (str(threadingCounter + 1) + "/" + str(length + threadingCounterDefault))
+        # proc = Process(target=ask_google, args=(phrase_list[i], threadingCounter,))
+        # procs.append(proc)
+        # proc.start()
         t1 = threading.Thread(target=ask_google, args=(phrase_list[i], threadingCounter,))
         t1.start()
-        print("There are currently " + str(threads) + " threads running")
+        # print("There are currently " + str(len(procs))  + " threads running")
+        print("There are currently " + str(threads)  + " threads running")
         threadingCounter += 1
         i += 1
     try:
+        # for proc in procs:
+        #     proc.join()
         t1.join()
     except UnboundLocalError:
         print("Tried to allow threads to close when none existed")
-
+    # print(time.time() - time_start)
     return threadingCounter - threadingCounterDefault
 
-def main(path_and_filename):
-    # Turn the path into its smaller parts to use them individually
-    path, filename = os.path.split(path_and_filename)
-    filename_no_ext, ext = os.path.splitext(filename)
-    # Get start time so that you can check how long it took
-    time1 = time.time()
 
-    # Open output.txt for writing
-    f = open("/home/c/github/Text-to-Speech/output.txt", 'w')
-    s = ""
-
-    '''This if and the subsaquint else statments extracts the text from
-    the file of the given format, splits it into small enough
-    pieces for it to go to to the text to speach api and then
-    multithreads the sending of the files to the api and saves the
-    replys from the api to mp3s'''
-    if (ext == ".pdf"):
-
-        pdfReader = PyPDF2.PdfFileReader(open(path_and_filename, 'rb'))
-        threadingCounter = 0
-
-        # Determin whether a pdf is made up of text or it is scanned in
-        x = 0
-        for i in range(5):
-            y = len(str(pdfReader.getPage(5 + i * 2).extractText()))
-            if x < y:
-                x = y;
-
-        if x > 65:
-            print("This is a normal pdf.")
-            for i in range(pdfReader.numPages):
-                # Extract text from pdf paeg
-                s = str(pdfReader.getPage(i).extractText())
-#               Wirte to output.txt
-                f.write(s)
-                f.flush()
-                # Turn into phrases small enough to send to api
-                ph = make_phrases(s)
-                # multithread and send to api
-                threadingCounter += make_threads(ph, threadingCounter)
-
-        else:
-            # Turn pdf into list of images
-            images = convert_from_path(path_and_filename)
-            for i in range(len(images)):
-                # Turn image that was a pdf page into an image
-                text = (image_pdf(images[i], i))
-                # Write to output.txt
-                f.write(text)
-                f.flush()
-                # Make page into small enough phrases to send to api
-                ph = make_phrases(text)
-                # Multithread and send to api
-                threadingCounter += make_threads(ph, threadingCounter)
-
-    elif ext == ".txt":
-        # Better for debuging. Make into one line when done
-        fr = open(path_and_filename, "r")
-        # Write to output.txt
-        f.write(fr.read())
-        f.flush()
-        # Make into smaller phrases to send to api for processing
-        ph = make_phrases(fr.read())
-        # Multi process the sending of the threads
-        make_threads(ph, 0)
-
-    elif ext == ".docx":
-#       Better for debuging when done put it in one function call
-        text = get_docx_text(path_and_filename)
-        # Write to output.txt
-        f.write(text)
-        f.flush()
-        ph = make_phrases(text)
-        make_threads(ph, 0)
-    # Reseting counter to 1
-    i = 1
+def make_full_track(filename_no_ext):
+    i = 0
     # Initalizeing the full track
     full_track = AudioSegment.from_mp3("/home/c/github/Text-to-Speech/Django/lolRipMe/media/unprocessed-audio/piece0.mp3")
     while os.path.isfile("/home/c/github/Text-to-Speech/Django/lolRipMe/media/unprocessed-audio/piece" + str(i) + ".mp3"):
@@ -200,8 +149,100 @@ def main(path_and_filename):
         full_track += new_track
         i += 1
     full_track.export("/home/c/github/Text-to-Speech/Django/lolRipMe/media/processed-audio/" + filename_no_ext + ".mp3", format='mp3')
-    # delete_old_files()
-    print("The time taken was " + str(time.time() - time1))
+
+
+def decide_pdfs(path_and_filename, filename_no_ext):
+    # Turn the path into its smaller parts to use them individually
+    path, filename = os.path.split(path_and_filename)
+    filename_no_ext, ext = os.path.splitext(filename)
+    # Initalize pdf reader
+    pdfReader = PyPDF2.PdfFileReader(open(path_and_filename, 'rb'))
+    # Determin whether a pdf is made up of text or it is scanned in
+    x = 0
+    for i in range(5):
+        y = len(str(pdfReader.getPage(5 + i * 2).extractText()))
+        if x < y:
+            x = y;
+    if x > 65:
+        print("This is a normal pdf.")
+        normal_pdf(path_and_filename, filename_no_ext)
+
+    else:
+        print("This is a scanned in pdf")
+        scanned_pdf(path_and_filename, filename_no_ext)
+
+
+def normal_pdf(path_and_filename, filename_no_ext):
+    threadingCounter = 0
+    pdfReader = PyPDF2.PdfFileReader(open(path_and_filename, 'rb'))
+    s = ''
+    f = open("/home/c/github/Text-to-Speech/Django/lolRipMe/media/text-files/" + filename_no_ext + ".txt", 'w')
+    for i in range(pdfReader.numPages):
+        # Extract text from pdf paeg
+        s = str(pdfReader.getPage(i).extractText())
+        # Wirte to output.txt
+        f.write(s)
+        f.flush()
+        # Turn into phrases small enough to send to api
+        ph = make_phrases(s)
+        # multithread and send to api
+        threadingCounter += make_threads(ph, threadingCounter)
+
+
+def scanned_pdf(path_and_filename, filename_no_ext):
+    f = open("/home/c/github/Text-to-Speech/Django/lolRipMe/media/text-files/" + filename_no_ext + ".txt", 'w')
+    threadingCounter = 0
+    # Turn pdf into list of images
+    images = convert_from_path(path_and_filename)
+    print(images)
+    image_to_text(images, path_and_filename)
+    # for i in range(len(images)):
+    #     # images[i] = images[i].convert('1')
+    #     # Turn image that was a pdf page into an image
+    #     time1 = time.time()
+    #     # text = (image_pdf(images[i], i))
+    #     print("it took " + str(time.time() - time1) + " seconds.")
+    #     # Write to output.txt
+    #     f.write(text)
+    #     f.flush()
+    #     # Make page into small enough phrases to send to api
+    #     ph = make_phrases(text)
+    #     # Multithread and send to api
+    #     threadingCounter += make_threads(ph, threadingCounter)
+
+
+def image(path_and_filename, filename_no_ext):
+    f = open("/home/c/github/Text-to-Speech/Django/lolRipMe/media/text-files//" + filename_no_ext + ".txt", 'w')
+    text = (image_pdf(images[i], i))
+    # Write to output.txt
+    f.write(text)
+    f.flush()
+    # Make page into small enough phrases to send to api
+    make_threads(make_phrases(text), 0)
+
+
+def txt(path_and_filename, filename_no_ext):
+    f = open("/home/c/github/Text-to-Speech/Django/lolRipMe/media/text-files//" + filename_no_ext + ".txt", 'w')
+    # Better for debuging. Make into one line when done
+    fr = open(path_and_filename, "r")
+    # Write to output.txt
+    f.write(fr.read())
+    f.flush()
+    # Make into smaller phrases to send to api for processing
+    ph = make_phrases(fr.read())
+    # Multi process the sending of the threads
+    make_threads(ph, 0)
+
+
+def docx(path_and_filename, filename_no_ext):
+    f = open("/home/c/github/Text-to-Speech/Django/lolRipMe/media/text-files/" + filename_no_ext + ".txt", 'w')
+#   Better for debuging when done put it in one function call
+    text = get_docx_text(path_and_filename, filename_no_ext)
+    # Write to output.txt
+    f.write(text)
+    f.flush()
+    ph = make_phrases(text)
+    make_threads(ph, 0)
 
 
 # List files in a directory going recursively.
@@ -229,80 +270,71 @@ def list_dirs(loc):
             dirlist.append(lol)
     return dirlist
 
-# Create your views here.
 
+def get_latest_file(path):
+    list_of_files = glob.glob(path + '/*')
+    latest_file = max(list_of_files, key=os.path.getctime)
+    return latest_file
+
+
+# Create your views here.
 def home(request):
-    f = open(('/home/c/github/Text-to-Speech/output.txt'), 'r')
-    file_content = make_phrases(f.read())
-    f.close()
     files = list_files('/home/c/github/Text-to-Speech/Django/lolRipMe/media/processed-audio')
     for i in range(len(files)):
         tmp, files[i] = os.path.split(files[i])
-    print(files)
     uploaded = get_latest_file("/home/c/github/Text-to-Speech/Django/lolRipMe/media/documents")
     context = {'file_content': file_content, 'download_list': files, 'latest_file': uploaded}
     return render(request, "home.html", context)
 
 
 def about(request):
-    f = open(('/home/c/github/Text-to-Speech/output.txt'), 'r')
-    file_content = make_phrases(f.read())
-    f.close()
     files = list_files('/home/c/github/Text-to-Speech/Django/lolRipMe/media/processed-audio')
     for i in range(len(files)):
         tmp, files[i] = os.path.split(files[i])
-    print(files)
     uploaded = get_latest_file("/home/c/github/Text-to-Speech/Django/lolRipMe/media/documents")
-    context = {'file_content': file_content, 'download_list': files, 'latest_file': uploaded}
+    context = {'download_list': files, 'latest_file': uploaded}
     return render(request, "about.html", context)
 
 
 def login(request):
-    f = open(('/home/c/github/Text-to-Speech/output.txt'), 'r')
-    file_content = make_phrases(f.read())
-    f.close()
     files = list_files('/home/c/github/Text-to-Speech/Django/lolRipMe/media/processed-audio')
     for i in range(len(files)):
         tmp, files[i] = os.path.split(files[i])
-    print(files)
     uploaded = get_latest_file("/home/c/github/Text-to-Speech/Django/lolRipMe/media/documents")
     context = {'file_content': file_content, 'download_list': files, 'latest_file': uploaded}
     return render(request, "login.html", context)
 
 
 def read(request):
-    f = open(('/home/c/github/Text-to-Speech/output.txt'), 'r')
-    file_content = make_phrases(f.read())
-    f.close()
+    # f = open(('/home/c/github/Text-to-Speech/Django/lolRipMe/media/text-files/" + filename_no_ext + ".txt"'), 'r')
+    # file_content = make_phrases(f.read())
+    # f.close()
     files = list_files('/home/c/github/Text-to-Speech/Django/lolRipMe/media/processed-audio')
     for i in range(len(files)):
         tmp, files[i] = os.path.split(files[i])
-    # print(files)
     uploaded = get_latest_file("/home/c/github/Text-to-Speech/Django/lolRipMe/media/documents")
-    context = {'file_content': file_content, 'download_list': files, 'latest_file': uploaded}
-    return render(request, "read.html", context)
-
-    # file_list_a = list_files("/home/c/github/Text-to-Speech/Django/lolRipMe/media/documents")
-    # if request.method == 'POST':
-    #     form = DocumentForm(request.POST, request.FILES)
-    #     if form.is_valid():
-    #         form.save()
-    #     file_list_b = list_files("/home/c/github/Text-to-Speech/Django/lolRipMe/media/documents")
-    #     p1 = threading.Thread(target=main, args=(x for x in file_list_b if x not in file_list_a),)
-    #     p1.start()
-    #     return redirect('read')
-    # else:
-    #     form = DocumentForm()
-    #     context = {'file_content': file_content, 'download_list': files, 'form': form}
-    #     return render(request, "read.html", context)
+    file_list_a = list_files("/home/c/github/Text-to-Speech/Django/lolRipMe/media/documents")
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            file_list_b = list_files("/home/c/github/Text-to-Speech/Django/lolRipMe/media/documents")
+            p1 = threading.Thread(target=main, args=(x for x in file_list_b if x not in file_list_a),)
+            p1.start()
+        return redirect('read')
+    else:
+        form = DocumentForm()
+        context = {'file_content': '', 'download_list': files, 'form': form}
+        return render(request, 'read.html', context)
 
 
 def text(request):
-    f = open(('/home/c/github/Text-to-Speech/output.txt'), 'r')
+    f = open(('/home/c/github/Text-to-Speech/Django/lolRipMe/media/text-files/" + filename_no_ext + ".txt"'), 'r')
     file_content = make_phrases(f.read())
     f.close()
     context = {'file_content': file_content}
     return render(request, "text.html", context)
+
 
 def sidenav(request):
     files = list_files('/home/c/github/Text-to-Speech/Django/lolRipMe/media/processed-audio')
@@ -312,16 +344,18 @@ def sidenav(request):
     context = {'download_list':files }
     return render(request, "sidenav.html", context)
 
+
 def model_form_upload(request):
-    f = open(('/home/c/github/Text-to-Speech/output.txt'), 'r')
+    f = open(('/home/c/github/Text-to-Speech/Django/lolRipMe/media/text-files/" + filename_no_ext + ".txt"'), 'r')
     file_content = make_phrases(f.read())
     f.close()
     files = list_files('/home/c/github/Text-to-Speech/Django/lolRipMe/media/processed-audio')
     for i in range(len(files)):
         tmp, files[i] = os.path.split(files[i])
-    print(files)
+    # print(files)
     uploaded = get_latest_file("/home/c/github/Text-to-Speech/Django/lolRipMe/media/documents")
     file_list_a = list_files("/home/c/github/Text-to-Speech/Django/lolRipMe/media/documents")
+
     if request.method == 'POST':
         form = DocumentForm(request.POST, request.FILES)
         if form.is_valid():
@@ -336,7 +370,28 @@ def model_form_upload(request):
         return render(request, 'model_form_upload.html', context)
 
 
-def get_latest_file(path):
-    list_of_files = glob.glob(path + '/*')
-    latest_file = max(list_of_files, key=os.path.getctime)
-    return latest_file
+def main(path_and_filename):
+    # Turn the path into its smaller parts to use them individually
+    path, filename = os.path.split(path_and_filename)
+    filename_no_ext, ext = os.path.splitext(filename)
+    # Erase Output.txt
+    f = open("/home/c/github/Text-to-Speech/Django/lolRipMe/media/text-files/" + filename_no_ext + ".txt", 'w')
+    f.close()
+    # Image types
+    image_types = {'.jpg', '.png', '.gif', '.jpeg', '.tif', '.raw'}
+
+    # Get start time so that you can check how long it took
+    time1 = time.time()
+    if (ext.lower() == ".pdf"):
+        decide_pdfs(path_and_filename, filename_no_ext)
+    elif ext.lower() == ".txt":
+        txt(path_and_filename, filename_no_ext)
+    elif ext.lower() == ".docx":
+        docx(path_and_filename, filename_no_ext)
+    elif ext.lower() in image_types:
+        image(path_and_filename, filename_no_ext)
+
+    # make_full_track(filename_no_ext)
+    # delete_old_files()
+
+    print("The time taken was " + str(time.time() - time1))
